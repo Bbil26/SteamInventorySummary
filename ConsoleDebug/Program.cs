ace5ad44cf2ea0590d1c486567f987ae73e9d1dd
Bbil26 <bbil26@vk.com>
2024-06-12 23:31:24 +0300

    удаление лишнего

--- ConsoleDebug/Program.cs
+++ ConsoleDebug/Program.cs
@@ -1,5 +1,4 @@
 ﻿using SteamAPI;
 using Newtonsoft.Json;
 
-string testurl = "https://steamcommunity.com/market/priceoverview/?currency=5&appid=730&market_hash_name=Dual%20Berettas%20%7C%20Contractor%20%28Battle-Scarred%29";
 InteractionAPI.Main();
